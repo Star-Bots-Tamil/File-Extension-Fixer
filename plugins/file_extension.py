e7ba84831b7911ad4fa2bdb43ad15a3a565b6baf
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-29 11:41:30 +0530

    Update file_extension.py

--- plugins/file_extension.py
+++ plugins/file_extension.py
@@ -1,7 +1,9 @@
 from pyrogram import Client, filters
+from database.database import db
 
-@Client.on_message(filters.text & filters.private & ~filters.command(['start','help','about','showthumbnail','deletethumbnail','set_caption','get_caption','del_caption','change_mode','get_mode','stats','restart','broadcast',]))
-def send_text(client, message):
+@Client.on_message(filters.text & filters.private & ~filters.command(['start','help','about','set_prefix','see_prefix','del_prefix','ban','unban','banned','get_mode','stats','restart','broadcast',]))
+def file_extension_fixer(client, message):
+    prefix = await db.get_prefix(message.from_user.id)
     user = message.text
     first_60_letters = user[:60]
     client.send_message(message.chat.id, f"`{first_60_letters}.mkv`")
