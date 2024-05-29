97d60db1cc68fb2ceb2527f8e45688bf44de0a44
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2024-05-29 11:35:39 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -141,3 +141,32 @@ async def cb_handler(client, query: CallbackQuery):
         except:
             await query.message.delete()
             await query.message.continue_propagation()
+
+# prefix commond ✨
+@Client.on_message(filters.private & filters.command('set_prefix'))
+async def add_prefix(client, message):
+    if len(message.command) == 1:
+        return await message.reply_text("**__Give The Prefix__\n\nExᴀᴍᴩʟᴇ:- `/set_prefix @Rkn_Bots`**")
+    prefix = message.text.split(" ", 1)[1]
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    await db.set_prefix(message.from_user.id, prefix)
+    await RknDev.edit("__**✅ ᴘʀᴇꜰɪx ꜱᴀᴠᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('del_prefix'))
+async def delete_prefix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await db.get_prefix(message.from_user.id)
+    if not prefix:
+        return await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+    await db.set_prefix(message.from_user.id, None)
+    await RknDev.edit("__**❌️ ᴘʀᴇꜰɪx ᴅᴇʟᴇᴛᴇᴅ**__")
+
+@Client.on_message(filters.private & filters.command('see_prefix'))
+async def see_prefix(client, message):
+    RknDev = await message.reply_text("Please Wait ...", reply_to_message_id=message.id)
+    prefix = await db.get_prefix(message.from_user.id)
+    if prefix:
+        await RknDev.edit(f"**ʏᴏᴜʀ ᴘʀᴇꜰɪx:-**\n\n`{prefix}`")
+    else:
+        await RknDev.edit("__**😔 ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴘʀᴇꜰɪx**__")
+      
